db660a36fc5fbe080a5c73077ca216dc7f414988
Rubanrk004 <69233309+Rubanrk004@users.noreply.github.com>
2024-07-26 17:18:08 +0530

    Update VideoTutorialPage.js

--- frontend/src/VideoTutorialPage.js
+++ frontend/src/VideoTutorialPage.js
@@ -10,7 +10,7 @@ const VideoTutorialPage = () => {
 
   const fetchVideos = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/avbadmin/video');
+      const response = await axios.get('https://failtofly-backend.onrender.com/avbadmin/video');
       setVideos(response.data);
     } catch (error) {
       console.error('Error fetching videos:', error);
