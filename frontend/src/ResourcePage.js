b98c6aad6734c18fe0265370ee4e22378e550043
Rubanrk004 <69233309+Rubanrk004@users.noreply.github.com>
2024-07-26 17:16:21 +0530

    Update ResourcePage.js

--- frontend/src/ResourcePage.js
+++ frontend/src/ResourcePage.js
@@ -9,7 +9,7 @@ const ResourcePage = () => {
   useEffect(() => {
     const fetchResources = async () => {
       try {
-        const response = await axios.get(`http://localhost:5000/adminres/${endpoint}`);
+        const response = await axios.get(`https://failtofly-backend.onrender.com/adminres/${endpoint}`);
         setResources(response.data);
       } catch (error) {
         console.error('Error fetching resources:', error);
