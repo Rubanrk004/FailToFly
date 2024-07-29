e7bceaf0c2fea3ec6c6cbd238622d903d7289809
Rubanrk004 <69233309+Rubanrk004@users.noreply.github.com>
2024-07-29 12:06:22 +0530

    Update AdminLogin.js

--- frontend/src/AdminLogin.js
+++ frontend/src/AdminLogin.js
@@ -1,4 +1,3 @@
-// src/components/AdminLogin.js
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -22,35 +21,37 @@ const AdminLogin = () => {
   };
 
   return (
-    <div className="bg-white p-8 rounded  w-full max-w-md">
-      <h1 className="text-2xl font-bold mb-4">Admin Login</h1>
-      {error && <p className="text-red-500 mb-4">{error}</p>}
-      <form onSubmit={(e) => { e.preventDefault(); handleLogin(); }}>
-        <div className="mb-4">
-          <label className="block text-gray-700">Username</label>
-          <input
-            type="text"
-            className="mt-1 p-2 border border-gray-300 rounded w-full"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700">Password</label>
-          <input
-            type="password"
-            className="mt-1 p-2 border border-gray-300 rounded w-full"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
-        >
-          Login
-        </button>
-      </form>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-green-400 to-blue-500/40">
+      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+        <h1 className="text-2xl font-bold mb-4">Admin Login</h1>
+        {error && <p className="text-red-500 mb-4">{error}</p>}
+        <form onSubmit={(e) => { e.preventDefault(); handleLogin(); }}>
+          <div className="mb-4">
+            <label className="block text-gray-700">Username</label>
+            <input
+              type="text"
+              className="mt-1 p-2 border border-gray-300 rounded w-full"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Password</label>
+            <input
+              type="password"
+              className="mt-1 p-2 border border-gray-300 rounded w-full"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
